f9db64ec74c7107b53543ef2d2d2c492c7811c03
unclebob482 <danelege@gmail.com>
2025-02-26 15:29:50 -0600

    Add files via upload

---
+++ AssassinPy-main/AssasinPy/Assassin Lancher.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Lanching Assassin
+python Assassin.py
---
+++ AssassinPy-main/AssasinPy/download.bat
@@ -0,0 +1,21 @@
+@echo off
+echo This will take a little bit.
+pip install torch==1.9.0+cu111 --find-links https://download.pytorch.org/whl/torch_stable.html
+pip install torchvision==0.10.0+cu111 --find-links https://download.pytorch.org/whl/torch_stable.html
+pip install matplotlib>=3.2.2
+pip install numpy>=1.18.5
+pip install opencv-python>=4.1.2
+pip install Pillow
+pip install PyYAML>=5.3.1
+pip install scipy>=1.4.1
+pip install tqdm>=4.41.0
+pip install tensorboard>=2.4.1
+pip install seaborn>=0.11.0
+pip install pandas
+pip install mss
+pip install pygam
+pip install pynput
+pip install pywin32
+pip install requests
+pip install wheel
+pip install termcolor
